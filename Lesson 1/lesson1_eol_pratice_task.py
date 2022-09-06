2d85cdfd1042c200139790d8b4e9f567c74987a5
wafflez5 <103608431+wafflez5@users.noreply.github.com>
2022-09-06 15:07:37 +1200

    Add files via upload

---
+++ Lesson 1/lesson1_eol_pratice_task.py
@@ -0,0 +1,30 @@
+# E M Petersen
+# Focus - variables, labels, comments, data types.
+
+# Variables
+
+
+# A summary of this information should be printed out to the screen.
+# output = print()
+
+
+# Clarify the problem
+# name (var)
+
+
+
+# height - expressed in metres
+
+
+# address
+# street number, street name, suburb, town, city,
+
+
+# phone number - landline or mobile
+# stored as a string - no calc
+
+# Allergy
+# Boolean 
+
+
+print()
