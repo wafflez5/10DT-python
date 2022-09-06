01070402ba9dd7956b50ef57463bbd61b5c2ba29
wafflez5 <103608431+wafflez5@users.noreply.github.com>
2022-09-06 15:08:51 +1200

    Add files via upload

---
+++ Lesson 1/basic_var_and_output.py
@@ -0,0 +1,14 @@
+# E M Petersen
+# Variables
+
+# Declaration of variables
+name = "Elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+# Output to screen
+print(name)
+print(numLegs)
+print(height)
+print(hasTail)
---
+++ Lesson 1/basic_var_and_output_with_labels.py
@@ -0,0 +1,18 @@
+# E M Petersen
+# Variables
+
+# Declaration of variables
+name = "Elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+# Output to screen
+print("One of the big five animals in Africa is called an",name,'.')
+print("An",name,"has",numLegs,"legs.")
+print("Its height is approximately",height,"metres.")
+print("And, yes, it's",hasTail,"it has a tail.")
+
+numlegs = 3
+print(numLegs)
+print(numlegs)
