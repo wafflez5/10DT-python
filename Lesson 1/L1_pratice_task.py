afa1b25ee791784c5b8fc2356e1901aebbe7e360
wafflez5 <103608431+wafflez5@users.noreply.github.com>
2022-09-08 09:33:31 +1200

    Add files via upload

---
+++ Lesson 1/L1_pratice_task.py
@@ -0,0 +1,9 @@
+# E M Petersen
+# Focus - variables, print, labels, comments
+
+# Variables
+name = "Eddie Petersen"
+height = 1.79
+address = "170 Coutts Street"
+phone = "049391234"
+hasAllergy = False
